919c2337da5009851dcc940f04c7536f9e4fcecc
Darien Labbe <115522115+darienlabbe@users.noreply.github.com>
2024-04-17 12:29:30 -0700

    updated files
    part of the updates for socket io functionality

--- motor-controller/src/App.js
+++ motor-controller/src/App.js
@@ -1,8 +1,26 @@
-import React from "react";
-import Calendar from "./util/calendar";
-import Menu from "./util/menu";
+import React, { useEffect } from "react";
+import Calendar from "./components/calendar";
+import Menu from "./components/menu";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3001");
 
 function App() { 
+  useEffect(() => {
+    // Event listener
+    socket.on('connect', () => {
+      // Recieve message from the server
+      socket.on("user connected", (data) => {
+        console.log("Server: ", data);
+      });
+    });
+
+    return () => {
+      // Remove event listener
+      socket.off("connect");
+    }
+  }, []);
+  
   return (
     <div className="grid grid-cols-1 gap-x-10 gap-y-10 border-gray-200">
       <div className="flex items-center justify-center min-w-0 min-h-28 border-b-4">
