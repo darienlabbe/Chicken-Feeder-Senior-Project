57428fd4b4e7dc42b4b965ebeafcacee59ccc588
Darien Labbe <115522115+darienlabbe@users.noreply.github.com>
2024-05-10 01:08:37 -0700

    Added comments

--- motor-controller/node-server/server.js
+++ motor-controller/node-server/server.js
@@ -41,9 +41,10 @@ const httpServer = createServer((req, res) => {
     }); 
 });
 
+// Create a new instance of a backend server
 const io = new Server(httpServer, { cors: { origin: "*" }});
 
-// Connect to database
+// Connect to the MySQL database
 const db = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
@@ -81,8 +82,9 @@ async function updateEventList () {
     const [result] = await db.query(`SELECT * FROM events`);
     const new_result = JSON.stringify(result, null, 4); 
 
+    // Write the data from the database into the events json file for ease of listing
     fs.writeFile("../src/components/event-list.json", new_result, 'utf8', function (err) {
-        if (err) return console.log(err);
+        if (err) return console.error(err);
     });
 
     return new_result;
@@ -94,6 +96,7 @@ async function updateOccurList () {
     const [result] = await db.query(`SELECT * FROM occurrences`);
     const new_result = JSON.stringify(result, null, 4); 
 
+    // Write the data from the database into the occurrences json file for ease of listing
     fs.writeFile("../src/components/occurrence-list.json", new_result, 'utf8', function (err) {
         if (err) return console.log(err);
     });
@@ -116,6 +119,7 @@ async function deleteEvent (id) {
 
 // Import the JSON file
 async function importOccurList() {
+    // Get the json file, parse it, and then check the occurrences to see if the motor should run
     const json_str = await fs.promises.readFile(occur_path, 'utf-8');
     occur_list = JSON.parse(json_str);
     await checkForOccur();
@@ -131,20 +135,19 @@ fs.watch(occur_path, () => {
 
 // Function to run immediatly when the backend is loaded but only once
 async function checkForOccur() {
+    // Get the current time and date
     const currentTime = new dayjs.tz();
     const today_date = new dayjs.tz().get('year') + "-" + (dayjs.tz().get('month') + 1).toString().padStart(2, '0') + "-" + dayjs.tz().get('date').toString().padStart(2, '0');
 
     // Check all items in the occurrence list
     occur_list.forEach((occur) => {
         if (today_date === occur.run_date.split('T')[0] && currentTime.get('hour').toString().padStart(2, '0') === occur.run_time.split(':')[0] && currentTime.get('minute').toString().padStart(2, '0') === occur.run_time.split(':')[1]) {
-            // Trigger the relay
-            console.log('Turn on the relay for', occur.amt_feed, 'secs');
-            const amt_mil = occur.amt_feed * 1000;
-            
             // Turn the relay on
+            console.log('Turn on the relay for', occur.amt_feed, 'secs');
             relay.writeSync(1);   
-
+            
             // Wait for the specified time
+            const amt_mil = occur.amt_feed * 1000;
             setTimeout(() => {
                 // Turn the relay off after the specified amount of time
                 relay.writeSync(0);
@@ -170,15 +173,17 @@ async function runEveryCurrMin() {
     }
 }
 
+// When the code starts, ensure the two json files are updated and start the checker
 await updateEventList();
 await updateOccurList();
 setInterval(runEveryCurrMin, 1000); // Run every second
 
+// Function that detects if a new user connected to the app
 io.on("connection", async (socket) => {
-    // Send a message to client that a new user connected
+    // Send a message back to the client that a new user connected
     socket.emit("user connected", "A new user has connected");
 
-    // Get input values
+    // Get the input values from the frontend and add that data to the database
     socket.on("new_feeding", async function(data) {
         // Get array data from frontend
         let run_imm = data[0];
@@ -191,16 +196,18 @@ io.on("connection", async (socket) => {
         let end_day = new dayjs.tz();
         let id = 0;
 
-        // Create variables for later data input
+        // Create variables for the current day, current time, and a future date for the cap on repeating forever
 	    let now = dayjs.tz().get('year') + "-" + (dayjs.tz().get('month') + 1).toString().padStart(2, '0') + "-" + dayjs.tz().get('date').toString().padStart(2, '0');
         let future = (dayjs.tz().get('year') + 50) + "-" + (dayjs.tz().get('month') + 1).toString().padStart(2, '0') + "-" + dayjs.tz().get('date').toString().padStart(2, '0');
         let currentTime = dayjs.tz().get('hour').toString().padStart(2, '0') + ':' + dayjs.tz().get('minute').toString().padStart(2, '0') + ':' + dayjs.tz().get('second').toString().padStart(2, '0');
         
-        // Change values for the two checkboxes 
+        // Check if the user selected run immediatly, if so, then change the start day and start time to now
         if (run_imm) {
             st_day = now;
             st_time = currentTime;
         }
+
+        // Check if the user selected repeat forever, if so, then change the end date to 50 years later than today
         if (repeat_f) {
             e_day = future;
         }
@@ -211,172 +218,209 @@ io.on("connection", async (socket) => {
                 // Set end_day to start_day for running once
                 e_day = st_day;
 
-                // Input event
+                // Input an event into the database
                 const insert_r1 = await createEvent(recure_interval, st_day, st_time, e_day, amount);
-                console.log(insert_r1);
 
-                // Calculate and input occurrence(s)
+                // Calculate and input occurrence, then update the json file
                 id = Object.values(insert_r1)[2];
                 await createOccurrence(id, st_day, st_time, amount);
                 await updateOccurList();
                 break;
             case "Everyday":
+                // Input the new event into the database
                 const insert_ed = await createEvent(recure_interval, st_day, st_time, e_day, amount);
-                //console.log(insert_ed);
-
+                
+                // Emit the loading state so that the loading screen can prevent a user 
+                // from doing further actions while the occurrences are created 
                 socket.emit("loading");
 
-                // Calculate and input occurrence(s)
+                // Get the event id for the foreign key and make the string date into a new dayjs date object
                 id = Object.values(insert_ed)[2];
                 end_day = new dayjs.tz(e_day);
+                
+                // Calculate and input the occurrences, then update the json file
                 for (let i = new dayjs.tz(st_day); i <= end_day; i = i.add(1, 'day')) {
                     let new_date = i.tz().year().toString() + "-" + (i.tz().month() + 1).toString() + "-" + i.tz().date().toString();
                     await createOccurrence(id, new_date, st_time, amount);
                 }
                 
-                // Call to set the list of occurrences file
+                // Call to set the list of occurrences file and emit the done loading screen state
                 await updateOccurList();
                 socket.emit("done_loading");
                 break;
             case "Every Other Day":
+                // Input the new event into the database    
                 const insert_eod = await createEvent(recure_interval, st_day, st_time, e_day, amount);
-                //console.log(insert_eod);
 
+                // Emit the loading state so that the loading screen can prevent a user 
+                // from doing further actions while the occurrences are created 
                 socket.emit("loading");
 
-                // Calculate and input occurrence(s)
+                // Get the event id for the foreign key and make the string date into a new dayjs date object
                 id = Object.values(insert_eod)[2];
                 end_day = new dayjs.tz(e_day);
+                
+                // Calculate and input the occurrences, then update the json file
                 for (let i = new dayjs.tz(st_day); i <= end_day; i = i.add(2, 'day')) {
                     let new_date = i.tz().year().toString() + "-" + (i.tz().month() + 1).toString() + "-" + i.tz().date().toString();
                     await createOccurrence(id, new_date, st_time, amount);
                 }
                 
-                // Call to set the list of occurrences file
+                // Call to set the list of occurrences file and emit the done loading screen state
                 await updateOccurList();
                 socket.emit("done_loading");
                 break;
             case "Once Every Week":
+                // Input the new event into the database
                 const insert_ew = await createEvent(recure_interval, st_day, st_time, e_day, amount);
-                //console.log(insert_ew);
 
+                // Emit the loading state so that the loading screen can prevent a user 
+                // from doing further actions while the occurrences are created 
                 socket.emit("loading");
 
-                // Calculate and input occurrence(s)
+                // Get the event id for the foreign key and make the string date into a new dayjs date object
                 id = Object.values(insert_ew)[2];
                 end_day = new dayjs.tz(e_day);
+                
+                // Calculate and input the occurrences, then update the json file
                 for (let i = new dayjs.tz(st_day); i <= end_day; i = i.add(1, 'week')) {
                     let new_date = i.tz().year().toString() + "-" + (i.tz().month() + 1).toString() + "-" + i.tz().date().toString();
                     await createOccurrence(id, new_date, st_time, amount);
                 }
                 
-                // Call to set the list of occurrences file
+                // Call to set the list of occurrences file and emit the done loading screen state
                 await updateOccurList();
                 socket.emit("done_loading");
                 break;
             case "Once Every Other Week":
+                // Input the new event into the database
                 const insert_eow = await createEvent(recure_interval, st_day, st_time, e_day, amount);
-                //console.log(insert_eow);
 
+                // Emit the loading state so that the loading screen can prevent a user 
+                // from doing further actions while the occurrences are created 
                 socket.emit("loading");
-                
-                // Calculate and input occurrence(s)
+
+                // Get the event id for the foreign key and make the string date into a new dayjs date object
                 id = Object.values(insert_eow)[2];
                 end_day = new dayjs.tz(e_day);
+                
+                // Calculate and input the occurrences, then update the json file
                 for (let i = new dayjs.tz(st_day); i <= end_day; i = i.add(2, 'week')) {
                     let new_date = i.tz().year().toString() + "-" + (i.tz().month() + 1).toString() + "-" + i.tz().date().toString();
                     await createOccurrence(id, new_date, st_time, amount);
                 }
 
-                // Call to set the list of occurrences file
+                // Call to set the list of occurrences file and emit the done loading screen state
                 await updateOccurList();
                 socket.emit("done_loading");
                 break;
             case "Once Every Month":
+                // Input the new event into the database
                 const insert_em = await createEvent(recure_interval, st_day, st_time, e_day, amount);
-                //console.log(insert_em);
 
+                // Emit the loading state so that the loading screen can prevent a user 
+                // from doing further actions while the occurrences are created 
                 socket.emit("loading");
 
-                // Calculate and input occurrence(s)
+                // Get the event id for the foreign key and make the string date into a new dayjs date object
                 id = Object.values(insert_em)[2];
                 end_day = new dayjs.tz(e_day);
+                
+                // Calculate and input the occurrences, then update the json file
                 for (let i = new dayjs.tz(st_day); i <= end_day; i = i.add(1, 'month')) {
                     let new_date = i.tz().year().toString() + "-" + (i.tz().month() + 1).toString() + "-" + i.tz().date().toString();
                     await createOccurrence(id, new_date, st_time, amount);
                 }
                 
-                // Call to set the list of occurrences file
+                // Call to set the list of occurrences file and emit the done loading screen state
                 await updateOccurList();
                 socket.emit("done_loading");
                 break;
             case "Once Every Other Month":
+                // Input the new event into the database
                 const insert_eom = await createEvent(recure_interval, st_day, st_time, e_day, amount);
-                //console.log(insert_eom);
 
+                // Emit the loading state so that the loading screen can prevent a user 
+                // from doing further actions while the occurrences are created 
                 socket.emit("loading");
 
-                // Calculate and input occurrence(s)
+                // Get the event id for the foreign key and make the string date into a new dayjs date object
                 id = Object.values(insert_eom)[2];
                 end_day = new dayjs.tz(e_day);
+                
+                // Calculate and input the occurrences, then update the json file
                 for (let i = new dayjs.tz(st_day); i <= end_day; i = i.add(2, 'month')) {
                     let new_date = i.tz().year().toString() + "-" + (i.tz().month() + 1).toString() + "-" + i.tz().date().toString();
                     await createOccurrence(id, new_date, st_time, amount);
                 }
                 
-                // Call to set the list of occurrences file
+                // Call to set the list of occurrences file and emit the done loading screen state
                 await updateOccurList();
                 socket.emit("done_loading");
                 break;
             case "Once Every 6 Months":
+                // Input the new event into the database
                 const insert_e6m = await createEvent(recure_interval, st_day, st_time, e_day, amount);
-                //console.log(insert_e6m);
 
+                // Emit the loading state so that the loading screen can prevent a user 
+                // from doing further actions while the occurrences are created 
                 socket.emit("loading");
 
-                // Calculate and input occurrence(s)
+                // Get the event id for the foreign key and make the string date into a new dayjs date object
                 id = Object.values(insert_e6m)[2];
                 end_day = new dayjs.tz(e_day);
+                
+                // Calculate and input the occurrences, then update the json file
                 for (let i = new dayjs.tz(st_day); i <= end_day; i = i.add(6, 'month')) {
                     let new_date = i.tz().year().toString() + "-" + (i.tz().month() + 1).toString() + "-" + i.tz().date().toString();
                     await createOccurrence(id, new_date, st_time, amount);
                 }
                 
-                // Call to set the list of occurrences file
+                // Call to set the list of occurrences file and emit the done loading screen state
                 await updateOccurList();
                 socket.emit("done_loading");
                 break;
             case "Once A Year":
+                // Input the new event into the database
                 const insert_y = await createEvent(recure_interval, st_day, st_time, e_day, amount);
-                //console.log(insert_y);
                 
+                // Emit the loading state so that the loading screen can prevent a user 
+                // from doing further actions while the occurrences are created 
                 socket.emit("loading");
-                
-                // Calculate and input occurrence(s)
+
+                // Get the event id for the foreign key and make the string date into a new dayjs date object
                 id = Object.values(insert_y)[2];
                 end_day = new dayjs.tz(e_day);
+                
+                // Calculate and input the occurrences, then update the json file
                 for (let i = new dayjs.tz(st_day); i <= end_day; i = i.add(1, 'year')) {
                     let new_date = i.tz().year().toString() + "-" + (i.tz().month() + 1).toString() + "-" + i.tz().date().toString();
                     await createOccurrence(id, new_date, st_time, amount);
                 }
                 
-                // Call to set the list of occurrences file
+                // Call to set the list of occurrences file and emit the done loading screen state
                 await updateOccurList();
                 socket.emit("done_loading");
                 break;
         }    
     });
 
-    // Function for recieveing the id to delete an event from frontend 
-    socket.on("delete_feeding", async function(data) {
-        console.log("Id to be deleted: ", data);
+    // Function for recieveing the id in order to delete an event 
+    socket.on("delete_feeding", async function(id) {
+        console.log("Id to be deleted: ", id);
+        
+        // Load while waiting for an event to delete
         socket.emit("loading");
-        const del = await deleteEvent(data);
+
+        // delete the event by referencing the id 
+        await deleteEvent(id);
+        
+        // When the deletion is done then stop showing the loading screen 
         socket.emit("done_loading");
-        console.log(del);
     });
 });
 
+// Listen on port local port 3001 
 httpServer.listen(3001, () => {
     console.log("listening on port 3001");
 });
